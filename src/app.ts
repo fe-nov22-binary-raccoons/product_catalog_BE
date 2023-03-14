f2108ee91023dad4a99b1f4976e25b25fb207079
Oleh Holovnykh <shankar.oleh@gmail.com>
2023-03-14 21:12:49 +0200

    add husky

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+function add(...numbers: number[]) {
+  let sum = 0;
+
+  for (const n of numbers) 
+  {
+    sum += n;
+
+  }
+
+  return sum;
+}
+
+// eslint-disable-next-line no-console
+console.log(add(1, 2, 3, 4));
