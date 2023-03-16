b6ce9c3a1525c3fa489f8d24cf66d3ca1299428a
maasiimka <mrozisalovec@gmail.com>
2023-03-16 11:11:18 +0200

    added get endpoint with pagination

--- dist/index.js
+++ dist/index.js
@@ -2,10 +2,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 import * as phoneControllers from './controller/phones.js';
 dotenv.config();
-
 const port = process.env.PORT || 3000;
 const app = express();
-
 app.get('/phones', express.json(), phoneControllers.getAll);
 app.listen(port);
-// # sourceMappingURL=index.js.map
+//# sourceMappingURL=index.js.map
