c23e71360a5adc38a42f2b6cfd789a4ad8d1b44d
svitjojo <kornotavr@gmail.com>
2023-03-16 10:54:31 +0200

    Database initialized, phones added to db

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import express, { Express } from 'express';
 import dotenv from 'dotenv';
+import { initDB } from './utils/initDB.js';
 
 dotenv.config();
 
 const port = process.env.PORT || 3000;
 
 const app: Express = express();
+
+initDB();
 
 app.get('/expenses', (req, res) => {
   res.send('<div>HSsdsd</div>');
