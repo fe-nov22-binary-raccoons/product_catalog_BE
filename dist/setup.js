eba968ccc3199d3717384037387c64e0473c399d
maasiimka <mrozisalovec@gmail.com>
2023-03-17 00:22:39 +0200

    save

--- dist/setup.js
+++ dist/setup.js
@@ -2,8 +2,8 @@ import { Item } from './models/Item.js';
 import { Phone } from './models/Phone.js';
 import { initDB } from './utils/initDB.js';
 (async () => {
-  initDB();
-  await Phone.sync({ alter: true });
-  await Item.sync({ alter: true });
+    initDB();
+    await Phone.sync({ alter: true });
+    await Item.sync({ alter: true });
 })();
 //# sourceMappingURL=setup.js.map
