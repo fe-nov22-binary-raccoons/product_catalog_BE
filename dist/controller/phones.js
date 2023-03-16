367ffe54ffad8093d165bd06836a30d80d93ec8d
maasiimka <mrozisalovec@gmail.com>
2023-03-16 11:15:14 +0200

    saving

--- dist/controller/phones.js
+++ dist/controller/phones.js
@@ -1,18 +1,26 @@
 import * as phoneServices from '../services/phones.js';
-export const getAll = async (req, res) => {
-    let { page, size } = req.query;
-    if (!page && !size) {
-        const phones = await phoneServices.getAll();
-        res.send(phones);
-        return;
-    }
-    if (!page) {
-        page = '1';
-    }
-    if (!size) {
-        size = '12';
-    }
-    const pageOfPhones = await phoneServices.getPage(+page, +size);
-    res.send(pageOfPhones);
+
+export const getAll = async(req, res) => {
+  let { page, size } = req.query;
+
+  if (!page && !size) {
+    const phones = await phoneServices.getAll();
+
+    res.send(phones);
+
+    return;
+  }
+
+  if (!page) {
+    page = '1';
+  }
+
+  if (!size) {
+    size = '12';
+  }
+
+  const pageOfPhones = await phoneServices.getPage(+page, +size);
+
+  res.send(pageOfPhones);
 };
-//# sourceMappingURL=phones.js.map
+// # sourceMappingURL=phones.js.map
--- dist/services/phones.js
+++ dist/services/phones.js
@@ -1,15 +1,19 @@
 import path from 'path';
 import { readFile } from 'fs/promises';
-export const getAll = async () => {
-    const data = await readFile(path.resolve('src', 'phones.json'));
-    const phones = JSON.parse(data.toString());
-    return phones;
+
+export const getAll = async() => {
+  const data = await readFile(path.resolve('src', 'phones.json'));
+  const phones = JSON.parse(data.toString());
+
+  return phones;
 };
-export const getPage = async (page, size) => {
-    const data = await readFile(path.resolve('src', 'phones.json'));
-    const phones = JSON.parse(data.toString());
-    const start = (page - 1) * size;
-    const end = page * size;
-    return phones.slice(start, end);
+
+export const getPage = async(page, size) => {
+  const data = await readFile(path.resolve('src', 'phones.json'));
+  const phones = JSON.parse(data.toString());
+  const start = (page - 1) * size;
+  const end = page * size;
+
+  return phones.slice(start, end);
 };
-//# sourceMappingURL=phones.js.map
+// # sourceMappingURL=phones.js.map
